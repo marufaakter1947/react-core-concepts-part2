b913af67866e87b879a7aea04d944a13c8bcaf2d
Marufa Akter <marufaakter1947@gmail.com>
2025-09-18 08:51:50 +0600

    Load dynamic data ,API call using use()

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import Counter from './counter';
 import './App.css'
 import Batsman from './Bartsman';
+import Users from './Users';
+import { Suspense } from 'react';
+import Friends from './Friends';
+// 1.just write a simple fetch with json conversion
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users").then(res=>res.json())
+
+const fetchFriends = async()=>{
+  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  return res.json();
+}
+
 
 function App() {
+
+  const friendsPromise = fetchFriends();
+
+
  function handleClick(){
   alert("I am clicked")
  }
@@ -20,6 +35,14 @@ alert(newNum);
     <>
       
       <h3>Vite + React</h3>
+      {/* // Wrap the data loading component under suspense */}
+      <Suspense fallback={<h3>Loading....</h3>}>
+      <Users fetchUsers={fetchUsers} ></Users>
+      </Suspense>
+
+      <Suspense fallback={<h3>Friends are coming for treat...</h3>}>
+<Friends friendsPromise={friendsPromise} ></Friends>
+      </Suspense>
 <Batsman></Batsman>
       <Counter></Counter>
 
